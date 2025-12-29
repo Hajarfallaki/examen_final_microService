315bae306e0e745d658467bc51aab7d9410a933c
Hajarfallaki <hajarfallaki24@gmail.com>
2025-12-29 09:00:43 +0100

    premier commit

---
+++ src/main/java/ma/enset/java/microserviceexamen/MicroServiceExamenApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.java.microserviceexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceExamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceExamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/java/microserviceexamen/MicroServiceExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.java.microserviceexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceExamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
